38f643b497aaf95a0efffae1375edaf50bbc1c18
Joao Vitor <vitor.1943@gmail.com>
2026-07-08 11:28:50 -0300

    initial commit

---
+++ src/test/java/dev/joaov/anime_api/AnimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaov.anime_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
