8a5565096629926d560c61c3dbd78919490f2fae
Joao Vitor <vitor.1943@gmail.com>
2026-07-10 13:50:01 -0300

    refactor: adds order for AnimeRepositoryTest

--- src/test/java/dev/joaov/animeapi/repository/AnimeRepositoryTest.java
+++ src/test/java/dev/joaov/animeapi/repository/AnimeRepositoryTest.java
@@ -3,13 +3,12 @@ package dev.joaov.animeapi.repository;
 import dev.joaov.animeapi.model.Anime;
 import dev.joaov.animeapi.model.AnimeStatus;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AnimeRepositoryTest {
     private AnimeRepository repository;
     private List<Anime> animeList;
@@ -24,12 +23,14 @@ class AnimeRepositoryTest {
 
     @Test
     @DisplayName("findAll return empty list when there are no animes")
+    @Order(1)
     void findAll_ReturnsEmptyList_WhenThereAreNoAnimes() {
         Assertions.assertThat(animeList).isNotNull().isEmpty();
     }
 
     @Test
     @DisplayName("findById find an existent anime")
+    @Order(2)
     void findById_ReturnsAnAnime_WhenAnimeIsFound() {
         Anime animeSaved = repository.save(hunterxhunter);
         var animeFound = repository.findById(animeSaved.getId());
@@ -39,6 +40,7 @@ class AnimeRepositoryTest {
 
     @Test
     @DisplayName("findById return Optional.empty() when anime doesnt exist")
+    @Order(3)
     void findById_ReturnsEmpty_WhenAnimeIsNotFound() {
         var id = 2L;
         var animeFound = repository.findById(id);
@@ -47,6 +49,8 @@ class AnimeRepositoryTest {
     }
 
     @Test
+    @DisplayName("save Saves an anime to the list")
+    @Order(4)
     void save_SavesAnAnime_WhenSuccessful() {
         Anime animeSaved = repository.save(hunterxhunter);
 
@@ -57,6 +61,7 @@ class AnimeRepositoryTest {
 
     @Test
     @DisplayName("update replaces an anime correctly")
+    @Order(5)
     void update_ReplacesAnAnime_WhenSuccessful() {
         var animeSaved = repository.save(hunterxhunter);
 
@@ -69,6 +74,7 @@ class AnimeRepositoryTest {
 
     @Test
     @DisplayName("delete removes an anime from the list")
+    @Order(6)
     void delete_RemovesAnAnime_WhenSuccessful() {
         var animeSaved = repository.save(hunterxhunter);
         repository.delete(animeSaved);
